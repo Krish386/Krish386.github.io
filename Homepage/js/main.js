04f10fd2a1d561e454351cbe08585233fff53caf
Yeet <75026067+Krish386@users.noreply.github.com>
2022-11-09 20:00:03 +0300

    Files Upload

---
+++ Homepage/js/main.js
@@ -0,0 +1,17 @@
+const tl= gsap.timeline({defaults:{ease: 'power1.out'}});
+
+tl.to('.text',{y:'0%', duration: 2, stagger: 0.2});
+tl.to('.slider',{y:'-100%', duration:1.5, delay:0.5});
+tl.to('.intro',{y:'-100%', duration:1}, '-=1')
+tl.fromTo('.title', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'});
+tl.fromTo('.logo', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'} ,'-=1');
+tl.fromTo('.quote', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'} ,'-=1');
+tl.fromTo('.sub_title', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'} ,'-=1');
+
+
+
+
+
+
+
+
