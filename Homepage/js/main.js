5be8dcdf467d10101693c363ab97639a42f8ceed
Yeet <75026067+Krish386@users.noreply.github.com>
2022-11-10 23:21:06 +0300

    Upload Files

--- Homepage/js/main.js
+++ Homepage/js/main.js
@@ -1,12 +1,26 @@
 const tl= gsap.timeline({defaults:{ease: 'power1.out'}});
 
 tl.to('.text',{y:'0%', duration: 2, stagger: 0.2});
-tl.to('.slider',{y:'-100%', duration:1.5, delay:0.5});
+tl.to('.slider',{y:'-100%', duration:1.5, delay:1.5});
 tl.to('.intro',{y:'-100%', duration:1}, '-=1')
 tl.fromTo('.title', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'});
 tl.fromTo('.logo', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'} ,'-=1');
+tl.fromTo('.scroll-down', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'} ,'-=1');
 tl.fromTo('.quote', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'} ,'-=1');
 tl.fromTo('.sub_title', {opacity: 0, y:'50%'}, {opacity: 1, duration: 1, y:'0%'} ,'-=1');
+
+var mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
+
+icon.onclick =function(){
+  if(mySong.paused){
+    mySong.play();
+    icon.src = ""
+  }else{
+    mySong.pause();
+    icon.src = ""
+  }
+}
 
 
 
